f4befaf09a8900c36b150b681e7c76c45b9b9407
Arthur Mascaro <arthumascaro69@gmail.com>
2023-01-05 15:59:56 -0300

    User UseCases Creation

--- src/main/java/edu/br/ifsp/domain/usecases/tools/FindToolUseCase.java
+++ src/main/java/edu/br/ifsp/domain/usecases/tools/FindToolUseCase.java
@@ -1,7 +1,6 @@
 package edu.br.ifsp.domain.usecases.tools;
 
 import edu.br.ifsp.domain.entities.tools.Tool;
-import edu.br.ifsp.domain.entities.tools.ToolItem;
 import edu.br.ifsp.domain.usecases.utils.Validator;
 
 import java.util.List;
@@ -11,25 +10,21 @@ import java.util.UUID;
 public class FindToolUseCase {
 
     private ToolDAO toolDAO;
-
     public FindToolUseCase(ToolDAO toolDAO) {
         this.toolDAO = toolDAO;
     }
-
     public Optional<Tool> findOne(UUID id) {
         if (id == null) {
             throw new IllegalArgumentException("ID cannot be null!");
         }
         return toolDAO.findOne(id);
     }
-
     public Optional<Tool> findByUUID(UUID id) {
         if (Validator.nullOrEmpty(id.toString())) {
             throw new IllegalArgumentException("Id cannot be null or empty!");
         }
         return toolDAO.findByUUID(id);
     }
-
     public List<Tool> findAll() {
         return toolDAO.findALL();
     }
---
+++ src/main/java/edu/br/ifsp/domain/usecases/user/FindUserUseCase.java
@@ -0,0 +1,38 @@
+package edu.br.ifsp.domain.usecases.user;
+
+import edu.br.ifsp.domain.entities.tools.Tool;
+import edu.br.ifsp.domain.entities.user.Promptuary;
+import edu.br.ifsp.domain.entities.user.User;
+import edu.br.ifsp.domain.usecases.user.UserDAO;
+import edu.br.ifsp.domain.usecases.utils.Validator;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class FindUserUseCase {
+
+    private UserDAO userDAO;
+
+    public FindUserUseCase(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public Optional<User> findOne(UUID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null!");
+        }
+        return userDAO.findOne(id.toString());
+    }
+
+    public Optional<User> findByPromptuary(Promptuary promptuary) {
+        if (Validator.nullOrEmpty(promptuary.toString())) {
+            throw new IllegalArgumentException("Promptuary cannot be null or empty!");
+        }
+        return userDAO.findByPromptuary(promptuary);
+    }
+
+    public List<User> findAll() {
+        return userDAO.findALL();
+    }
+}
