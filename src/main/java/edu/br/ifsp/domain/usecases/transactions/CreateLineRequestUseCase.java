9871d11d6f7698eb3086f21adf8313ba1739cb93
renan823 <renan.trofino@gmail.com>
2023-03-23 18:53:55 -0300

    Feat: LineRequestInMemoryDAO

--- src/main/java/edu/br/ifsp/domain/usecases/transactions/CreateLineRequestUseCase.java
+++ src/main/java/edu/br/ifsp/domain/usecases/transactions/CreateLineRequestUseCase.java
@@ -4,9 +4,15 @@ import edu.br.ifsp.domain.entities.tools.ToolItem;
 import edu.br.ifsp.domain.entities.tools.ToolSituation;
 import edu.br.ifsp.domain.entities.transaction.LineRequest;
 import edu.br.ifsp.domain.entities.transaction.Request;
+import edu.br.ifsp.domain.entities.user.Promptuary;
+import edu.br.ifsp.domain.entities.user.User;
 import edu.br.ifsp.domain.usecases.tools.FindToolItemUseCase;
 import edu.br.ifsp.domain.usecases.tools.UpdateToolItemUseCase;
+import edu.br.ifsp.domain.usecases.user.UserInputRequestValidator;
+import edu.br.ifsp.domain.usecases.utils.EntityAlreadyExistsException;
 import edu.br.ifsp.domain.usecases.utils.EntityNotFoundException;
+import edu.br.ifsp.domain.usecases.utils.Notification;
+import edu.br.ifsp.domain.usecases.utils.Validator;
 
 import javax.sound.sampled.Line;
 import java.util.UUID;
@@ -32,14 +38,14 @@ public class CreateLineRequestUseCase {
         this.updateToolItemUseCase = updateToolItemUseCase;
     }
 
-    public LineRequest createALineRequest(UUID requestID, UUID toolItemID) throws LineRequestNotAllowedException {
-        if (toolItemID == null){
-            throw new IllegalArgumentException("Tool Item ID is null.");
-        }
+    public CreateLineRequestUseCase(LineRequestDAO lineRequestDAO){ this.lineRequestDAO = lineRequestDAO; }
 
-        if (requestID == null){
+    public LineRequest createLineRequest(UUID requestID, UUID toolItemID) throws LineRequestNotAllowedException {
+        if (toolItemID == null)
+            throw new IllegalArgumentException("Tool Item ID is null.");
+
+        if (requestID == null)
             throw new IllegalArgumentException("Request ID is null.");
-        }
 
         ToolItem toolItem = findToolItemUseCaseUseCase.findOne(toolItemID).orElseThrow(() ->
                 new EntityNotFoundException("Can not find a Tool Item with ID" + toolItemID));
@@ -47,12 +53,25 @@ public class CreateLineRequestUseCase {
         Request request = findRequestUseCase.findOne(requestID).orElseThrow(() ->
                 new EntityNotFoundException("Can not find a Request with ID" + requestID));
 
-        if(toolItem.getSituation() == ToolSituation.BUSY) {
+        if(toolItem.getSituation() == ToolSituation.BUSY)
             throw new LineRequestNotAllowedException("The Tool Item with ID" + toolItemID + "is unavailable.");
-        }
 
-            LineRequest lineRequest = new LineRequest();
-            return lineRequestDAO.create(lineRequest);
+        LineRequest lineRequest = new LineRequest();
+        return lineRequestDAO.create(lineRequest);
+    }
 
+    public LineRequest insert(LineRequest lineRequest){
+        Validator<LineRequest> validator = new LineRequestInputRequestValidator();
+        Notification notification = validator.validate(lineRequest);
+
+        UUID id = lineRequest.getId();
+
+        if (lineRequestDAO.findByUUID(id).isPresent())
+            throw new EntityAlreadyExistsException("This ID is already in use");
+        
+        if (notification.hasErros())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        return lineRequestDAO.create(lineRequest);
     }
 }
