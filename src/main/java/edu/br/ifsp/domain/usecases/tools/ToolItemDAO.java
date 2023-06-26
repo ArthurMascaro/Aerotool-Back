10412b332b527160e0b1dcf08bce0e7d399ebd42
Arthur Mascaro <arthumascaro69@gmail.com>
2023-06-26 12:21:40 -0300

    feat: locate new methods

--- src/main/java/edu/br/ifsp/domain/usecases/tools/ToolItemDAO.java
+++ src/main/java/edu/br/ifsp/domain/usecases/tools/ToolItemDAO.java
@@ -1,8 +1,10 @@
 package edu.br.ifsp.domain.usecases.tools;
 
+import edu.br.ifsp.domain.entities.tools.Locate;
 import edu.br.ifsp.domain.entities.tools.ToolItem;
 import edu.br.ifsp.domain.usecases.utils.DAO;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,5 +12,10 @@ public interface ToolItemDAO extends DAO<ToolItem, UUID> {
 
     // TODO Implements Patrimony UNIQUE at Database
     // Optional<ToolItem> findByPatrimony(String patrimony);
+    Locate createLocate(Locate locate);
+
+    List<Locate> findAllLocates();
+
+    Optional<Locate> findLocateById(UUID id);
 
 }
